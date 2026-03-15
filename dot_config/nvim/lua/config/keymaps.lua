ef24e08ae6a6d33e42f60dc27e66e4614507d645
Gunsrude <gunsrude@stellar.build>
2026-03-15 13:26:02 -0500

    Add nowait

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -21,5 +21,6 @@ keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<CR>", { desc = "List buffe
 keymap.set("n", "<leader>bd", "<cmd>Bdelete<CR>", { desc = "Delete buffer" })
 
 --fuckit button
-keymap.set("n", "qqq", "<cmd>qa<CR>", { desc = "Quit Entirely" })
+--keymap.set("n", "qqq", "<cmd>qa<CR>", { desc = "Quit Entirely" })
+keymap.set("n", "qqq", "<cmd>qa<CR>", { nowait = true, desc = "Quit Entirely" })
 
