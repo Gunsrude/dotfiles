973b5e9eec58075c623d437afbf42b714fc373ea
Gunsrude <gunsrude@stellar.build>
2026-05-26 15:22:48 -0500

    nvim & tmux tweaks

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -53,6 +53,7 @@ return {
 
   {
     "nvim-telescope/telescope.nvim",
+    branch = "master",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter-textobjects",
